784e2e0e8e15f53919b6b57393e1b185ed348700
Sebastien Anty <bibianty@gmail.com>
2021-10-01 14:45:35 -0400

    full CRUD

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ admin.initializeApp({
 
 const db = admin.firestore();
 const clothesRef = db.collection("clothes");
-const handleErr = (err) => console.error
+const handleErr = (err) => console.error;
+const remapDoc = (doc) => {
+    let item = doc.data()
+    item.id = doc.id
+    return item
+}
 
 function createClothes() {
   const newClothes = {
@@ -29,23 +34,42 @@ function createClothes() {
     .catch(handleErr);
 }
 
+
+
 // delete "STRM04"
 
-clothesRef.doc("STRM04").delete()
-.then(() => {
-    clothesRef.get()
-        .then(collection => {
-            let clothes = []
-            collection.docs.forEach(doc => clothes.push(doc.data()))
-            console.log(clothes)
+// clothesRef.doc("STRM04").delete()
+// .then(() => {
+//     clothesRef.get()
+//         .then(collection => {
+//             let clothes = []
+//             collection.docs.forEach(doc => clothes.push(doc.data()))
+//             console.log(clothes)
+//         })
+// })
+
+// clothesRef.where("price", ">", 9.99).get()
+// .then(collection => {
+//     const clothes= collection.docs.map(remapDoc)
+//     console.log(clothes)
+// })
+// .catch(handleErr)
+
+// Get all shirts where price is < 30 
+clothesRef
+// .where("type", "==", "shirt")
+    .where("price", "<", 30)
+    .get()
+    .then(collection => {
+        collection.docs.forEach(doc => {
+            console.log("Updating price on", doc.id)
+            // update price = price * 1.25
+            const oldPrice = doc.data().price
+            const newPrice = oldPrice * 1.25
+
+            clothesRef.doc(doc.id)
+                .update({ price: newPrice, updated: true })
+                .then(() => console.log("Updated!"))
         })
-})
-
-
-clothesRef.get()
-  .then(collection => {
-    let clothes = []
-    collection.docs.forEach(doc => clothes.push(doc.data()))
-    console.log(clothes)
-  })
-  .catch(handleErr);
+    })
+    .catch(handleErr)
